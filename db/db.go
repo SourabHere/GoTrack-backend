17782b8917bcb1df45c1bfe918a518fad06ab451
Pranav <pranavtiwari12j@gmail.com>
2024-02-08 01:36:51 +0530

    Setup neon db

--- db/db.go
+++ db/db.go
@@ -4,42 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
-	"strconv"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
-
-var (
-	host     string
-	port     string
-	user     string
-	password string
-	dbname   string
-)
-
-func init() {
-
-	err := godotenv.Load()
-
-	if err != nil {
-		fmt.Println("Error: ", err.Error())
-	}
-
-	host = os.Getenv("HOST")
-	port = os.Getenv("PORT")
-	user = os.Getenv("USER")
-	password = os.Getenv("PASSWORD")
-	dbname = os.Getenv("DBNAME")
-}
-
-type DatabaseConfig struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	DBName   string
-}
 
 type Database interface {
 	InitDB() (*sql.DB, error)
@@ -50,22 +17,12 @@ type PostgresDB struct {
 }
 
 func NewPostgresDB() (*sql.DB, error) {
-
-	port, err := strconv.Atoi(port)
-
-	if err != nil {
-		return nil, fmt.Errorf("error converting port to integer: %w", err)
-	}
-
-	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	connectionString := os.Getenv("CONNECTION_STRING")
 
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		return nil, fmt.Errorf("error connecting to PostgreSQL database: %w", err)
 	}
-
-	// db.SetMaxOpenConns(10)
-	// db.SetMaxIdleConns(5)
 
 	err = db.Ping()
 
